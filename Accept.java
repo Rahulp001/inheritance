d5a681e74f7ab1350a4e844fe29c73106cab99f5
Rahulp001 <103584608+Rahulp001@users.noreply.github.com>
2022-06-09 18:12:49 +0530

    Add files via upload

---
+++ Accept.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+import java.util.Scanner;
+
+public class Accept {
+	int num1,num2;
+	public void use() 
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers: ");
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		
+	}
+}
