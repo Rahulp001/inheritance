1fdf7c052eb0ab39c3f2594eefcc74ecc3376620
Rahulp001 <103584608+Rahulp001@users.noreply.github.com>
2022-06-09 18:14:43 +0530

    Update Accept.java

--- Accept.java
+++ Accept.java
@@ -14,3 +14,99 @@ public class Accept {
 		
 	}
 }
+
+package inheritance;
+
+public class Child1 extends Accept{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child1 obj=new Child1();
+		obj.sum();
+	}
+	
+	public void sum() 
+	{
+		use();
+		int res;
+		res=num1+num2;
+		System.out.println("addition is "+res);
+	}
+
+}
+package inheritance;
+
+public class Child2 extends Accept{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child2 obj=new Child2();
+		obj.sub();
+
+	}
+	public void sub() 
+	{
+		use();
+		int res;
+		res=num1-num2;
+		System.out.println("substraction is "+res);
+	}
+
+}
+package inheritance;
+
+public class Child3 extends Accept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child3 obj=new Child3();
+		obj.mul();
+
+	}
+	public void mul() 
+	{
+		use();
+		int res;
+		res=num1*num2;
+		System.out.println("multiplication is "+res);
+	}
+
+}
+package inheritance;
+
+public class Child4 extends Accept{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child4 obj=new Child4();
+		obj.div();
+
+	}
+	public void div() 
+	{
+		use();
+		int res;
+		res=num1/num2;
+		System.out.println("division is "+res);
+	}
+
+}
+package inheritance;
+
+public class Child5 extends Accept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child5 obj=new Child5();
+		obj.mod();
+
+	}
+	public void mod() 
+	{
+		use();
+		int res;
+		res=num1%num2;
+		System.out.println("mod is "+res);
+	}
+
+}
